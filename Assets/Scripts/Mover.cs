9677f774076dd14cfc82fb794ddd2e59ae632a14
porridgeknight <kevscode@hotmail.com>
2021-07-25 21:45:09 +0100

    implement interface for dependency inversion

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,12 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using RPG.Combat;
 using RPG.Core;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour
+    public class Mover : MonoBehaviour, IAction
     {
         // [SerializeField] Transform target;
 
@@ -27,7 +26,6 @@ namespace RPG.Movement
         public void StartMoveAction(Vector3 destination)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            GetComponent<Fighter>().Cancel();
             MoveTo(destination);
         }
 
@@ -37,7 +35,7 @@ namespace RPG.Movement
             navMeshAgent.isStopped = false;
         }
 
-        public void Stop()
+        public void Cancel()
         {
             navMeshAgent.isStopped = true;
         }
