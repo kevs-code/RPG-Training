a6703747fce72c32a95bf56823c49b00293a6f18
porridgeknight <kevscode@hotmail.com>
2021-07-25 12:27:24 +0100

    movement fixed has cycled dependancy

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+using RPG.Combat;
 namespace RPG.Movement
 {
     public class Mover : MonoBehaviour
     {
-        [SerializeField] Transform target;
+        // [SerializeField] Transform target;
 
         NavMeshAgent navMeshAgent;
 
@@ -22,9 +22,16 @@ namespace RPG.Movement
             UpdateAnimator();
         }
 
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
         public void MoveTo(Vector3 destination)
         {
             navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
         }
 
         public void Stop()
