bb515a71d752ffbb922964d11cdfbb5d7d2a5991
porridgeknight <kevscode@hotmail.com>
2021-09-01 22:13:06 +0100

    fader without persistance and spawn cross reference

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -9,8 +9,14 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
@@ -21,6 +27,12 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneToLoad < 0)
+            {
+                Debug.Log("Scene to load not set.");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
@@ -35,6 +47,7 @@ namespace RPG.SceneManagement
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this) continue;
+                if (portal.destination != destination) continue;
 
                 return portal;
             }
