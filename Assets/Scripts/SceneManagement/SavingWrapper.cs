42ab3052661e06486b8af18a4c10694e7efff3c6
porridgeknight <kevscode@hotmail.com>
2021-10-13 09:47:40 +0100

    changed fader and saving wrapper to lifecycle Awake

--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -11,7 +11,12 @@ namespace RPG.SceneManagement
         const string defaultSaveFile = "save";
         [SerializeField] float fadeInTime = 0.2f;
 
-        IEnumerator Start()
+        private void Awake()
+        {
+            StartCoroutine(LoadLastScene());
+        }
+
+        private IEnumerator LoadLastScene()
         {
             Fader fader = FindObjectOfType<Fader>();
             fader.FadeOutImmediate();
