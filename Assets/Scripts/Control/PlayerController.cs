2b1d763d74e4e3eb627d77e66edd1768f379fd4c
porridgeknight <kevscode@hotmail.com>
2021-10-20 20:15:18 +0100

    added configurable cursors
    none, movement, combat

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,12 +2,30 @@ using RPG.Movement;
 using RPG.Combat;
 using UnityEngine;
 using RPG.Attributes;
+using System;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         Health health;
+
+        enum CursorType
+        {
+            None,
+            Movement,
+            Combat
+        }
+
+        [System.Serializable]
+        struct CursorMapping
+        {
+            public CursorType type;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] CursorMapping[] cursorMappings = null;
 
         private void Awake()
         {
@@ -20,7 +38,8 @@ namespace RPG.Control
 
             if(InteractWithCombat()) return;
             if(InteractWithMovement()) return;
-            // print("Nothing left");
+
+            SetCursor(CursorType.None);
         }
 
         private bool InteractWithCombat()
@@ -37,6 +56,7 @@ namespace RPG.Control
                 {
                     GetComponent<Fighter>().Attack(target.gameObject);
                 }
+                SetCursor(CursorType.Combat);
                 return true; // cursor affordance onHover
             }
             return false;
@@ -53,9 +73,28 @@ namespace RPG.Control
                 {
                     GetComponent<Mover>().MoveTo(hit.point, 1f);
                 }
+                SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
+        }
+
+        private void SetCursor(CursorType type)
+        {
+            CursorMapping mapping = GetCursorMapping(type);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType type)
+        {
+            foreach (CursorMapping mapping in cursorMappings)
+            {
+                if (mapping.type == type)
+                {
+                    return mapping;
+                }
+            }
+            return cursorMappings[0];
         }
 
         private static Ray GetMouseRay()
