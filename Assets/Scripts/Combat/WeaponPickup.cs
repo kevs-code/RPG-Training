aacaacde8f46a3c614e004bb86b726c70b6f1ab2
porridgeknight <kevscode@hotmail.com>
2021-10-29 17:10:55 +0100

    cursor type throught iraycastable

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -48,5 +48,10 @@ namespace RPG.Combat
             }
             return true;
         }
+
+        public CursorType GetCursorType()
+        {
+            return CursorType.Pickup;
+        }
     }
 }
