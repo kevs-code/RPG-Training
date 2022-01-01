6bd27c181d6a0378a66b9071ee67df5b5e2a8885
porridgeknight <kevscode@hotmail.com>
2022-01-01 21:32:30 +0000

    Added Mic Boom audio listener to camera and health pickup hack

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -54,8 +54,6 @@ namespace RPG.Attributes
 
         public void TakeDamage(GameObject instigator, float damage)
         {
-            print(gameObject.name + " took damage: " + damage);
-
             healthPoints.value = Mathf.Max(healthPoints.value - damage, 0);
 
             if (healthPoints.value == 0)
@@ -69,6 +67,13 @@ namespace RPG.Attributes
                 takeDamage.Invoke(damage);
             }
         }
+
+
+        public void Heal(float healthToRestore)
+        {
+            healthPoints.value = Mathf.Min(healthPoints.value + healthToRestore, GetMaxHealthPoints());
+        }
+
 
         public float GetHealthPoints()
         {
