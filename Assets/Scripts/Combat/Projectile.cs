47311fa4fb74f6cdae89853a5990a8f555db2614
porridgeknight <kevscode@hotmail.com>
2022-02-17 13:33:06 +0000

    Directional targeting
    Swapped out projectile cs for gamedevtv projectile cs also

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,33 +1,32 @@
-using RPG.Attributes;
 using UnityEngine;
+using RPG.Attributes;
 using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] float speed = 8;
+        [SerializeField] float speed = 1;
         [SerializeField] bool isHoming = true;
         [SerializeField] GameObject hitEffect = null;
         [SerializeField] float maxLifeTime = 10;
         [SerializeField] GameObject[] destroyOnHit = null;
-        [SerializeField] float lifeAfterImpact = 2f;
+        [SerializeField] float lifeAfterImpact = 2;
         [SerializeField] UnityEvent onHit;
 
         Health target = null;
+        Vector3 targetPoint;
         GameObject instigator = null;
         float damage = 0;
 
         private void Start()
         {
-            if (target == null) return;
             transform.LookAt(GetAimLocation());
         }
 
         void Update()
         {
-            if (target == null) return;
-            if (isHoming && !target.IsDead())
+            if (target != null && isHoming && !target.IsDead())
             {
                 transform.LookAt(GetAimLocation());
             }
@@ -36,7 +35,18 @@ namespace RPG.Combat
 
         public void SetTarget(Health target, GameObject instigator, float damage)
         {
+            SetTarget(instigator, damage, target);
+        }
+
+        public void SetTarget(Vector3 targetPoint, GameObject instigator, float damage)
+        {
+            SetTarget(instigator, damage, null, targetPoint);
+        }
+
+        public void SetTarget(GameObject instigator, float damage, Health target = null, Vector3 targetPoint = default)
+        {
             this.target = target;
+            this.targetPoint = targetPoint;
             this.damage = damage;
             this.instigator = instigator;
 
@@ -45,6 +55,10 @@ namespace RPG.Combat
 
         private Vector3 GetAimLocation()
         {
+            if (target == null)
+            {
+                return targetPoint;
+            }
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
             if (targetCapsule == null)
             {
@@ -55,9 +69,11 @@ namespace RPG.Combat
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.GetComponent<Health>() != target) return;
-            if (target.IsDead()) return;
-            target.TakeDamage(instigator, damage);
+            Health health = other.GetComponent<Health>();
+            if (target != null && health != target) return;
+            if (health == null || health.IsDead()) return;
+            if (other.gameObject == instigator) return;
+            health.TakeDamage(instigator, damage);
 
             speed = 0;
 
@@ -74,6 +90,9 @@ namespace RPG.Combat
             }
 
             Destroy(gameObject, lifeAfterImpact);
+
         }
+
     }
+
 }
