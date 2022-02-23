ca68b8c76b81a4218997fc9577b06270dd8af248
porridgeknight <kevscode@hotmail.com>
2022-02-23 20:49:39 +0000

    Pause Menu UI and pause using timescale

---
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,28 @@
+using RPG.Control;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class PauseMenuUI : MonoBehaviour
+    {
+        PlayerController playerController;
+        private void Awake()
+        {
+            playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
+
+        private void OnEnable()
+        {
+            Time.timeScale = 0;
+            playerController.enabled = false;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1;
+            playerController.enabled = true;
+        }
+    }
+}
